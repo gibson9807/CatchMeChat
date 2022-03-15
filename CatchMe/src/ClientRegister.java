6459e03fc2458e897513d14be429a8ae0bae9bb1
Edyta Pałka <edyta1998r@gmail.com>
2022-03-15 16:32:49 +0100

    Refaktoryzacja kodu-wstepna

--- CatchMe/src/ClientRegister.java
+++ CatchMe/src/ClientRegister.java
@@ -1,9 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ClientRegister extends JFrame {
@@ -22,36 +20,29 @@ public class ClientRegister extends JFrame {
             this.setContentPane(mainPanel);
             this.pack();
             button.addActionListener(this::idButtonActionPerformed);
-
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-
     }
 
     public void idButtonActionPerformed(ActionEvent e) {
         try {
             String id = idText.getText();
-            Socket s = new Socket("localhost", 8000);
-            DataInputStream din = new DataInputStream(s.getInputStream());
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            Socket socket = new Socket("localhost", 8080);
+            //DataInputStream din = new DataInputStream(socket.getInputStream());
+            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
             dout.writeUTF(id);
-            String i = new DataInputStream(s.getInputStream()).readUTF();
+            String i = new DataInputStream(socket.getInputStream()).readUTF();
             if (i.equals("Jesteś już zarejestrowany")) {
                 JOptionPane.showMessageDialog(this, "Jesteś już zarejestrowany\n");
             } else {
-                new Client(id, s,"Klient "+id).setVisible(true);
+                new Client(id, socket, "Klient " + id).setVisible(true);
                 this.dispose();
             }
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
     }
-
 
     public static void main(String[] args) {
         JFrame frame = new ClientRegister("CatchMe Client Register");
